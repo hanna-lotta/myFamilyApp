a9f3d2dbae57c4c6536a613a90852409b1b90ee3
Hanna Selden <hanna_lotta@hotmail.com>
2026-02-09 19:15:09 +0100

    dynamoDb

---
+++ srcServer/data/dynamoDb.ts
@@ -0,0 +1,19 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+
+const accessKey: string = process.env.ACCESS_KEY || ''
+const secret: string = process.env.SECRET_ACCESS_KEY || ''
+
+const client: DynamoDBClient = new DynamoDBClient({
+	region: "eu-north-1",  
+	credentials: {
+		accessKeyId: accessKey,
+		secretAccessKey: secret,
+	},
+});
+const db: DynamoDBDocumentClient = DynamoDBDocumentClient.from(client);
+
+const tableName = 'MyFamilyApp'
+
+export { db, tableName }
